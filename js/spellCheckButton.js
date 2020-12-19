933c9192dd794c09dfa4eab64c3d6ec40f497e2d
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-12-18 20:08:38 -0800

    script to add/delete suggested word nodes

---
+++ js/spellCheckButton.js
@@ -0,0 +1,33 @@
+
+
+let checkSpelling = () => {
+    let wordsContainer = document.getElementById('suggested-words')
+    console.log('checking spelling ... ');
+    let wordNode = createSuggestion('new word');
+    wordsContainer.appendChild(wordNode)
+}
+
+let createSuggestion = (word) => {
+    let colDiv = document.createElement('div');
+    colDiv.setAttribute('class', 'word col-md-6');
+
+    let wordDiv = document.createElement('div');
+    wordDiv.setAttribute('class', 'p-3 border bg-light');
+    wordDiv.textContent = word;
+
+    colDiv.appendChild(wordDiv);
+
+    return colDiv;
+
+}
+
+let deleteSuggestions = () => {
+    let wordsContainer = document.getElementById('suggested-words');
+    let words = wordsContainer.querySelectorAll('.word');
+    let wordsArr = Array.from(words);
+    for (let word of wordsArr){
+        word.remove();
+    }
+    
+    console.log(words);
+}
